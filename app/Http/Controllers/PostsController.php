4adc262dfbc0845538e94269822d387cd6c388a9
SuYiXuan <3a832012@gm.student.ncut.edu.tw>
2021-11-29 21:01:19 +0800

    練習 8：前台路由

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -1,17 +1,27 @@
 <?php
 
 namespace App\Http\Controllers;
+use App\Models\Post;
+use Illuminate\Http\Request;
 
 class PostsController extends Controller
 {
     public function index()
     {
-        return view('posts.index');
+        $posts = Post::orderBy('id','DESC')->get();
+
+        $data =[
+            'posts' =>$posts
+        ];
+
+        return view('posts.index',$data);
     }
 
     public function show($id)
     {
-        $data = ['id' => $id];
+        $post=Post::find($id);
+
+        $data = ['post' => $post];
 
         return view('posts.show', $data);
     }
